2bd76ba50fa381de5084b0491f4ff3e76f6fb979
john-ngugi <johnngugi0407@gmail.com>
2024-03-17 12:01:56 +0300

    satelite Image

--- eemapper/views.py
+++ eemapper/views.py
@@ -7,7 +7,6 @@ import ee
 def index(request):
     
     Map = geemap.Map() 
-    
     # load our image collection 
     
     
@@ -34,20 +33,35 @@ def index(request):
         )
 
         return image.updateMask(mask).divide(10000)
-
+     
+     
+    ROI = ee.FeatureCollection('projects/ee-muthamijohn/assets/Taita_Taveta')
 
     sentinelCollection = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")\
     .filterDate('2017-01-01','2023-12-30')\
-    .filterMetadata('CLOUDY_PIXEL_PERCENTAGE','less_than',15).map(mask_s2_clouds)
+    .filterMetadata('CLOUDY_PIXEL_PERCENTAGE','less_than',1).map(mask_s2_clouds)\
+    .filterBounds(ROI)
     
+    
+    Map.centerObject(sentinelCollection,10)
+    
+    sentinel_image = sentinelCollection.median().clip(ROI)
     sentinel_vis_params = {
         'min': 0,
         'max':0.3,
         'bands':['B4','B3','B2']
     }
     
-    Map.addLayer(sentinelCollection,sentinel_vis_params,'sentinel image')
+    ndvi = sentinel_image.normalizedDifference(['B4','B8'])
+    ndvi_vis_params= {
+        'min':-1,
+        'max':1,
+        'palette':['red','yellow','green']
+    }
     
+    Map.addLayer(sentinelCollection,sentinel_vis_params,'sentinel collection')
+    Map.addLayer(sentinel_image,sentinel_vis_params,'sentinel image')
+    Map.addLayer(ndvi,ndvi_vis_params,'ndvi')
     
     context = {
     'map': Map.to_html()
